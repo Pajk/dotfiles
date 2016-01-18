5c5da13311e1c9a6f22e849fa7fa283c4124eb42
Pavel Pokorny <pajkycz@gmail.com>
2016-01-18 18:55:03 +0100

    install dependencies added

---
+++ dependencies.sh
@@ -0,0 +1,7 @@
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew install tmux
+brew install the_silver_searcher
+brew install node
+npm install -g eslint
+npm install -g eslint_d
+
